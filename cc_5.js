9d4e3b85e42e633c659377cce68274b2df871ba1
Alikhan <fixator2004@gmail.com>
2025-02-09 21:56:31 -0500

    Task 5 - Product Availability.

--- cc_5.js
+++ cc_5.js
@@ -51,3 +51,12 @@ return prices.map(price => price * 0.9)};
 
 let finalPrices = discountPrice(prices);    //declared a variable to store the output from discount price function
 console.log(`Discount Prices: $${finalPrices}`); //logged the discounted prices
+
+
+//Task 5: Filter Method
+
+let inventory = [0, 5, 10, 20, 1];  //declared an object with 5 product quantities
+
+let updated_inventory = inventory.filter(stock => stock > 0);   //use .filter() method to remove products for which stock is 0
+
+console.log(`Updated Inventory: ${updated_inventory}`); //logged the array with updated stock quantities
