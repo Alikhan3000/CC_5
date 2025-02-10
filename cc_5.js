321c124ffacbe94f39f95da3aff941889612c054
Alikhan <fixator2004@gmail.com>
2025-02-09 21:00:14 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// Task 1: Object Properties
+
+let  customer = {           //declared an object with 3 properties
+
+    name: "John Doe", 
+    age: 35,
+    email: "john.doe@mail.com"
+};
+
+console.log(`Name:${customer.name}`);   //logged each property of the object using template literal
+console.log(`Age:${customer.age}`);
+console.log(`Email:${customer.email}`);
+
