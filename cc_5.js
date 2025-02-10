63bf91cf3932d9a05bc8b4337062b8b6e186c06b
Alikhan <fixator2004@gmail.com>
2025-02-09 21:16:16 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -11,3 +11,21 @@ console.log(`Name:${customer.name}`);   //logged each property of the object usi
 console.log(`Age:${customer.age}`);
 console.log(`Email:${customer.email}`);
 
+//Task 2: Object Methods
+
+let order = {       //declared an object with 3 properties and a method inside the object
+
+    orderId: 12345, 
+    totalAmount: 150,
+    status: "Processing",
+
+    //declared a method to log the order details using this, which referes to the object itself
+    displayOrder: function (){          
+        console.log(`Order Id: ${this.orderId}`);
+        console.log(`Total amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+    
+};
+
+order.displayOrder();   //called the method to log order details
